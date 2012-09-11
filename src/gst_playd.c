44549a530b9933c4af93f1e954944dc64a46160b
Paul Betts <paul@paulbetts.org>
2012-09-10 18:29:22 -0700

    Implement some option parsing

--- src/gst_playd.c
+++ src/gst_playd.c
@@ -20,16 +20,32 @@
 
 #include <stdio.h>
 #include <sys/types.h>
-#include <getopt.h>
+
+#include <glib.h>
+#include <glib/goption.h>
 
 #define EXIT_FAILURE 1
 
-/* The name the program was run with, stripped of any leading path. */
-char *program_name;
+static gboolean verbose = FALSE;
+static char* client_message = NULL;
 
-int
-main (int argc, char **argv)
+static GOptionEntry entries[] = {
+	 { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Be verbose", NULL },
+	 { "send-message", 's', 0, G_OPTION_ARG_STRING, &client_message, "Send a message to a running gst_playd and exit", NULL },
+};
+
+int main (int argc, char **argv)
 {
-	printf("Foo!\n");
-	exit(0);
+	GError* err = NULL;
+	GOptionContext* ctx;
+
+	ctx = g_option_context_new(" - A GStreamer backend daemon for Play");
+	g_option_context_add_main_entries(ctx, entries, "");
+	if (!g_option_context_parse(ctx, &argc, &argv, &err)) {
+		g_error("Option parsing failed: %s", err->message);
+		return 1;
+	}
+
+	g_option_context_free(ctx);
+	return 0;
 }

db0a661dbb148e1adcb619dded81b4f6aba8a11a
Paul Betts <paul@paulbetts.org>
2012-12-21 16:45:35 -0800

    Build out the basics of the audio device plugin

--- src/operations/audio_dev_osx.c
+++ src/operations/audio_dev_osx.c
@@ -1,8 +1,70 @@
+/*
+   audio_dev_osx.c - Audio device operations for OS X
+
+   Copyright (C) 2012 Paul Betts
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include <glib.h>
+#include <string.h>
+
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreAudio/AudioHardware.h>
 
-CFArrayRef CreateInputDeviceArray()
+#include "parser.h"
+#include "ping.h"
+#include "utility.h"
+#include "op_services.h"
+
+#include "operations/audio_dev_osx.h"
+
+static struct message_dispatch_entry audio_dev_messages[] = { 
+	{ "LISTDEVICE", op_listdevice_parse },
+	{ NULL },
+};
+
+void* op_audiodev_new(void* op_services)
 {
+	return op_services;
+}
+
+char* op_listdevice_parse(const char* param, void* ctx)
+{
+}
+
+gboolean op_audiodev_register(void* ctx, struct message_dispatch_entry** entries)
+{
+	if (!ctx) {
+		return FALSE;
+	}
+
+	*entries = audio_dev_messages;
+	return TRUE;
+}
+
+void op_audiodev_free(void* ctx)
+{
+}
+
+GHashTable* enumerate_audio_devices(gboolean only_input_devices, GError** error)
+{
+	/* This code is based on
+	 * http://stackoverflow.com/questions/4575408/audioobjectgetpropertydata-to-get-a-list-of-input-devices
+	 */
+
 	AudioObjectPropertyAddress propertyAddress = { 
 		kAudioHardwarePropertyDevices, 
 		kAudioObjectPropertyScopeGlobal, 
@@ -101,11 +163,11 @@ CFArrayRef CreateInputDeviceArray()
 		CFStringRef values  []  = { deviceUID,              deviceName,             deviceManufacturer };
 
 		CFDictionaryRef deviceDictionary = CFDictionaryCreate(kCFAllocatorDefault, 
-				(const void **)(keys), 
-				(const void **)(values), 
-				3,
-				&kCFTypeDictionaryKeyCallBacks,
-				&kCFTypeDictionaryValueCallBacks);
+			(const void **)(keys), 
+			(const void **)(values), 
+			3,
+			&kCFTypeDictionaryKeyCallBacks,
+			&kCFTypeDictionaryValueCallBacks);
 
 
 		CFArrayAppendValue(inputDeviceArray, deviceDictionary);

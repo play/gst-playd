647e9ec8ae6e08ee904dbc26cf18c27479facb17
Paul Betts <paul@paulbetts.org>
2013-01-15 15:09:08 -0800

    Restrict this to OS X only

--- src/gst_playd.c
+++ src/gst_playd.c
@@ -37,7 +37,10 @@
 #include "operations/ping.h"
 #include "operations/control.h"
 #include "operations/play.h"
+
+#if HAVE_APPLE_MEDIA
 #include "operations/audio_dev_osx.h"
+#endif
 
 #define EXIT_FAILURE 1
 
@@ -66,7 +69,9 @@ static struct parser_plugin_entry parser_operations[] = {
 	{ "Ping", NULL, op_ping_new, op_ping_register, op_ping_free },
 	{ "Control", NULL, op_control_new, op_control_register, op_control_free },
 	{ "Playback", NULL, op_playback_new, op_playback_register, op_playback_free },
+#if HAVE_APPLE_MEDIA
 	{ "AudioDevices", NULL, op_audiodev_new, op_audiodev_register, op_audiodev_free },
+#endif
 	{ NULL },
 };
 
